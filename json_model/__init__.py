8c4bbb32acd69a0caa3610c780d739d764e509b9
simon <simon.emmott@yahoo.co.uk>
2019-07-20 22:09:38 +0100

    Added filter, append and create

--- json_model/__init__.py
+++ json_model/__init__.py
@@ -5,9 +5,11 @@ class DoesNotExist(Exception):
 
 
 class ManagedListIterator(object):
-    def __init__(self, gen):
+    def __init__(self, gen, **kw):
         self.__gen = gen
         self.__length = None
+        self.__criteria = kw
+        self.__filtered = None
         
     def __iter__(self):
         self.__index = 0
@@ -15,12 +17,29 @@ class ManagedListIterator(object):
     
     def __next__(self):
         if self.__index >= self.__len__():
-            raise StopIteration
+            raise StopIteration       
         item = self.__gen[self.__index]
         self.__index = self.__index + 1
+        if self.__criteria:
+            if not matches(item, **self.__criteria):
+                item = self.__next__()
         return item
     
+    def _apply_filter(self):
+        count = 0
+        self.__filtered = []
+        for item in self.__gen:
+            if matches(item, **self.__criteria):
+                self.__filtered.append(item)
+                count = count + 1
+        self.__length = count
+        return count
+    
     def _get_length(self):
+        if self.__criteria:
+            if self.__filtered:
+                return self.__length
+            return self._apply_filter()  
         return len(self.__gen)
 
     def __len__(self):
@@ -30,21 +49,69 @@ class ManagedListIterator(object):
     def __getitem__(self, index):
         if index < 0 or index >= self.__len__():
             raise IndexError('Index [{index}] out of range'.format(index=index))
+        if self.__criteria:
+            if not self.__filtered:
+                self._apply_filter()
+            return self.__filtered[index]
         return self.__gen[index]
 
+def matches(item, **kw):
+    for key, value in kw.items():
+        if hasattr(item, key):
+            if value != getattr(item, key):
+                return False
+        else:
+            raise AttributeError('The object of type: {type} has no attribute {attr}'.format(
+                    type = type(item),
+                    attr = key
+                ))
+    return True
 
 class ListManager(object):
     
-    def __init__(self, gen):
-        self.__gen = gen
+    def __init__(self, data, **kw):
+        self.__data = data
+        self.__type = kw.get('type', None)
+        self.__length = None
+        
+    def _get_length(self):
+        return len(self.__data)
+
+    def __len__(self):
+        self.__length = self.__length if self.__length else self._get_length()
+        return self.__length
         
     def all(self):
-        return ManagedListIterator(self.__gen)
+        return ManagedListIterator(self.__data)
     
     def get(self, item=None, **kw):
         if item:
-            for i in self.__gen:
+            for i in self.__data:
                 if item == i:
                     return i
             raise DoesNotExist('The item: {item} does not exist in the supplied generator'.format(item=item))
+        else:
+            for i in self.__data:
+                if matches(i, **kw):
+                    return i
+            raise DoesNotExist('The supplied generator does not include an item with keys {keys}'.format(
+                    keys = kw
+                ))
+               
+    def filter(self, **kw):
+        return ManagedListIterator(self.__data, **kw)
+    
+    def append(self, item):
+        self.__data.append(item)
+        if self.__length:
+            self.__length = self.__length + 1
+        return item
         
+    def create(self, *args, **kw):
+        if not self.__type:
+            raise TypeError('No type defined for ListManager')
+        new = self.__type(*args, **kw)
+        return self.append(new)
+            
+            
+            
--- testing/list_manager_tests.py
+++ testing/list_manager_tests.py
@@ -1,6 +1,11 @@
 from unittest import TestCase
 from json_model import ListManager, DoesNotExist
 
+class Dummy(object):
+    def __init__(self, **kw):
+        for key, value in kw.items():
+            setattr(self, key, value)
+
 class ListManagerTests(TestCase):
 
     def test_new_instance(self):
@@ -32,4 +37,122 @@ class ListManagerTests(TestCase):
         self.assertEqual('c', lm.get('c'))
         with self.assertRaises(DoesNotExist):
             lm.get('z')
+        
+    def test_get_with_key(self):
+        data = [
+            Dummy(id=1, name='NAME_1'),
+            Dummy(id=2, name='NAME_2'),
+            Dummy(id=3, name='NAME_3')
+        ]
+        lm = ListManager(data)
+        self.assertTrue(isinstance(lm.get(id=1), Dummy))
+        self.assertEqual(1, lm.get(id=1).id)
+        self.assertEqual('NAME_1', lm.get(id=1).name)
+        self.assertTrue(isinstance(lm.get(id=2), Dummy))
+        self.assertEqual(2, lm.get(id=2).id)
+        self.assertEqual('NAME_2', lm.get(id=2).name)
+        self.assertTrue(isinstance(lm.get(id=3), Dummy))
+        self.assertEqual(3, lm.get(id=3).id)
+        self.assertEqual('NAME_3', lm.get(id=3).name)
+        with self.assertRaises(DoesNotExist):
+            lm.get(id=4)
+        with self.assertRaises(AttributeError):
+            lm.get(xxx=1)
+        self.assertTrue(isinstance(lm.get(name='NAME_1'), Dummy))
+        self.assertEqual(1, lm.get(name='NAME_1').id)
+        self.assertEqual('NAME_1', lm.get(name='NAME_1').name)
+
+    def test_get_with_keys(self):
+        data = [
+            Dummy(id=1, name='NAME_A'),
+            Dummy(id=2, name='NAME_A'),
+            Dummy(id=3, name='NAME_B'),
+            Dummy(id=4, name='NAME_B')
+        ]
+        lm = ListManager(data)
+        self.assertTrue(isinstance(lm.get(id=1, name='NAME_A'), Dummy))
+        self.assertEqual(1, lm.get(id=1, name='NAME_A').id)
+        self.assertEqual('NAME_A', lm.get(id=1, name='NAME_A').name)
+        with self.assertRaises(DoesNotExist):
+            lm.get(id=3, name='NAME_A')
+
+    def test_filter_with_keys(self):
+        data = [
+            Dummy(id=1, name='NAME_A'),
+            Dummy(id=2, name='NAME_A'),
+            Dummy(id=3, name='NAME_B'),
+            Dummy(id=4, name='NAME_B')
+        ]
+        lm = ListManager(data)
+        
+        self.assertEqual(1, len(lm.filter(id=1, name='NAME_A')))
+        self.assertTrue(isinstance(lm.filter(id=1, name='NAME_A')[0], Dummy))
+        self.assertEqual(1, lm.filter(id=1, name='NAME_A')[0].id)
+        self.assertEqual('NAME_A', lm.filter(id=1, name='NAME_A')[0].name)
+
+        self.assertEqual(2, len(lm.filter(name='NAME_A')))
+        self.assertTrue(isinstance(lm.filter(name='NAME_A')[0], Dummy))
+        self.assertEqual(1, lm.filter(name='NAME_A')[0].id)
+        self.assertEqual('NAME_A', lm.filter(name='NAME_A')[0].name)
+        self.assertTrue(isinstance(lm.filter(name='NAME_A')[1], Dummy))
+        self.assertEqual(2, lm.filter(name='NAME_A')[1].id)
+        self.assertEqual('NAME_A', lm.filter(name='NAME_A')[1].name)
+        
+    def test_length(self):
+        data = [
+            Dummy(id=1, name='NAME_A'),
+            Dummy(id=2, name='NAME_A'),
+            Dummy(id=3, name='NAME_B'),
+            Dummy(id=4, name='NAME_B')
+        ]
+        lm = ListManager(data)
+        self.assertEqual(4, len(lm))
+        
+
+    def test_append(self):
+        data = [
+            Dummy(id=1, name='NAME_A'),
+            Dummy(id=2, name='NAME_A'),
+            Dummy(id=3, name='NAME_B'),
+            Dummy(id=4, name='NAME_B')
+        ]
+        lm = ListManager(data)
+        lm.append(Dummy(id=5, name='NAME_C'))
+        self.assertEqual(5, len(lm))
+        self.assertEqual(5, lm.get(id=5).id)
+        self.assertEqual('NAME_C', lm.get(id=5).name)
+        self.assertEqual(1, len(lm.filter(name='NAME_C')))
+        self.assertEqual(5, lm.filter(name='NAME_C')[0].id)
+        self.assertEqual('NAME_C', lm.filter(name='NAME_C')[0].name)
+        
+    def test_create_with_no_type(self):
+        data = [
+            Dummy(id=1, name='NAME_A'),
+            Dummy(id=2, name='NAME_A'),
+            Dummy(id=3, name='NAME_B'),
+            Dummy(id=4, name='NAME_B')
+        ]
+        lm = ListManager(data)
+        with self.assertRaisesRegex(TypeError, 'No type defined for ListManager'):
+            lm.create(id=5, name='CREATED')
+
+    def test_create_with_type(self):
+        data = [
+            Dummy(id=1, name='NAME_A'),
+            Dummy(id=2, name='NAME_A'),
+            Dummy(id=3, name='NAME_B'),
+            Dummy(id=4, name='NAME_B')
+        ]
+        lm = ListManager(data, type=Dummy)
+        created = lm.create(id=5, name='CREATED')
+        self.assertIsNotNone(created)
+        self.assertTrue(isinstance(created, Dummy))
+        self.assertEqual(5, created.id)
+        self.assertEqual('CREATED', created.name)
+        self.assertEqual(5, len(lm))
+        got = lm.get(id=5)
+        self.assertEqual(5, got.id)
+        self.assertEqual('CREATED', got.name)
+
+
         
